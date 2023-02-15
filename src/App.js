67fa096b3754d46ce90e851d2b618f7e27d15e4a
avishesh karki <avishesh09401kr@gmail.com>
2023-02-15 15:01:36 +0545

    Dots are added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import "./App.css";
 import SingleReview from "./components/SingleReview";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { GoPrimitiveDot } from "react-icons/go";
 import reviews from "./assets/data";
 import { useEffect, useState } from "react";
 
 function App() {
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const showPrev = () => {
-    setActiveIndex(activeIndex - 1);
+  const showPrevSlide = () => {
+    setActiveIndex((prev) => prev - 1);
   };
 
-  const showNext = () => {
-    setActiveIndex(activeIndex + 1);
+  const showNextSlide = () => {
+    setActiveIndex((prev) => prev + 1);
   };
 
   useEffect(() => {
-    let slider = setInterval(() => {
-      setActiveIndex(activeIndex + 1);
-    }, 3000);
-
-    return () => clearInterval(slider);
+    activeIndex < 0 && setActiveIndex(reviews.length - 1);
+    activeIndex > reviews.length - 1 && setActiveIndex(0);
   }, [activeIndex]);
 
   useEffect(() => {
-    if (activeIndex < 0) {
-      setActiveIndex(reviews.length - 1);
-    } else if (activeIndex > reviews.length - 1) {
-      setActiveIndex(0);
-    }
+    const slide = setInterval(() => {
+      setActiveIndex((prev) => prev + 1);
+    }, 3000);
+
+    return () => clearInterval(slide);
   }, [activeIndex]);
 
   return (
@@ -39,22 +37,40 @@ function App() {
       <div className="reviews">
         {reviews.map((review, index) => {
           let position = "nextSlide";
-          if (index === activeIndex) {
-            position = "activeSlide";
-          } else if (
-            index === activeIndex - 1 ||
-            (activeIndex === 0 && index === reviews.length - 1)
-          ) {
-            position = "lastSlide";
-          }
-          return <SingleReview review={review} position={position} />;
+
+          index === activeIndex
+            ? (position = "activeSlide")
+            : index === activeIndex - 1 ||
+              (activeIndex === 0 && index === reviews.length - 1)
+            ? (position = "lastSlide")
+            : (position = "nextSlide");
+
+          return (
+            <div>
+              <SingleReview key={index} review={review} position={position} />
+            </div>
+          );
         })}
-        <span className="prev-container" onClick={showPrev}>
+        <span className="prev-container" onClick={showPrevSlide}>
           <FaChevronLeft className="prev" />
         </span>
-        <span className="next-container" onClick={showNext}>
+        <span className="next-container" onClick={showNextSlide}>
           <FaChevronRight className="next" />
         </span>
+        <div className="dot-container">
+          {reviews.map((review, index) => {
+            let color = "inactive";
+            if (index === activeIndex) {
+              color = "active";
+            }
+            return (
+              <GoPrimitiveDot
+                className={`dot ${color}`}
+                onClick={() => setActiveIndex(index)}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
